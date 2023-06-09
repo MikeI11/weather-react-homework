ef816e8bade2eb2135149a3110b1c45d1887bc1c
MikeI11 <132368319+MikeI11@users.noreply.github.com>
2023-06-10 00:42:30 +0300

    deleted axios and logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import Weather from "./Weather";
-import axios from "axios";
 
 function App() {
   return (
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import { Audio } from 'react-loader-spinner'
 import "./Weather.css";
 
